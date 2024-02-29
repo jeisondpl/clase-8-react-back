518ab99ac787600d3ff5c87d0fb70ecf05779059
jeison diaz palmera <jadiazp@indracompany.com>
2024-02-29 18:04:43 -0500

    init back

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+
+const app = express()
+
+const todo = [
+  {
+    id: 1,
+    title: 'Faire les courses',
+    done: false,
+  },
+  {
+    id: 2,
+    title: 'Faire les courses',
+    done: false,
+  },
+  {
+    id: 3,
+    title: 'Faire les courses',
+    done: false,
+  },
+  {
+    id: 4,
+    title: 'Faire les courses',
+    done: false,
+  },
+  {
+    id: 5,
+    title: 'Faire les courses',
+    done: false,
+  },
+  {
+    id: 6,
+    title: 'Faire les courses',
+    done: false,
+  },
+]
+
+app.get('/', (req, res) => {
+  res.json(todo)
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log('Server indra started on http://localhost:3000')
+})
